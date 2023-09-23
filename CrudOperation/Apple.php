66823862514b2f10ec65b3fa4273fdea0be3ac6e
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-23 16:48:42 +0545

    dfasdfs

--- CrudOperation/Apple.php
+++ CrudOperation/Apple.php
@@ -21,6 +21,7 @@ if(isset($_POST['submit'])){
     <title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   </head>
   <body>
     <form>
