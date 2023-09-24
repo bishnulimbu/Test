32eaefa0673ca12e77276eaa0d088e1bd5dfe131
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-24 10:16:52 +0545

    dfadsf

--- CrudOperation/Apple.php
+++ CrudOperation/Apple.php
@@ -2,9 +2,12 @@
 include 'connect.php';
 if(isset($_POST['submit'])){
   $name= $_POST['name'];
+  $email= $_POST['email'];
+  $phone= $_POST['phone'];
+  $gender= $_POST['gender'];
 
-  $sql="INSERT INTO Table1(name)
-  values('$name')";
+  $sql="INSERT INTO Table2(name, email, phone, gender)
+  values('$name','$email','$phone','$gender')";
 
   $result=mysqli_query($con, $sql);
   if($result){
@@ -20,19 +23,32 @@ if(isset($_POST['submit'])){
   <head>
     <title></title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<!-- <style> -->
+<!--     body{ -->
+<!--       margin: 0; -->
+<!--       padding: 0; -->
+<!--       display: flex; -->
+<!--       justify-content: center; -->
+<!--       height: 90vh; -->
+<!--       align-items:center; -->
+<!--     } -->
+<!-- </style> -->
   </head>
   <body>
-    <form>
-   <form method="post">
+    <div class="center-form">
+   <form>
+          <br>
     <label for="name" placeholder="Write your name">Name</label>
-    <input type="text" name="name">
-        <button type="submit" name="sumbit">Submit</button>
+    <input type="text" name="name"><br><br>
+    <label for="email" placeholder="Write your email">Email</label>
+    <input type="text" name="email"><br><br>
+    <label for="phone" placeholder="Write your phone no">phone</label>
+    <input type="number" name="phone"><br><br>
+    <label for="gender" placeholder="Write your gender">gender</label>
+    <input type="text" name="gender"><br><br>
+        <button type="submit" name="submit">Submit</button>
       </form>
-
-  
+        </div>
   </body>
 </html> 
-
 
